9f7f9a004d705cdf5e76fef0a7e29c3f8d0b20c1
Gabriel Barreto <gabriel.devloper@gmail.com>
2020-03-27 03:09:33 -0300

    front-end do app, finalizado.

--- frontend/src/pages/NovoCaso/index.js
+++ frontend/src/pages/NovoCaso/index.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
+import api from '../../services/api';
 
 import './styles.css';
 
 function NovoCaso() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('');
+
+    const ongId = localStorage.getItem('ongId');
+
+    const history = useHistory();
+
+    async function handleNovoCaso(e){
+        e.preventDefault();
+
+        const data = {
+            title,
+            description,
+            value
+        }
+
+        try {
+            
+            await api.post('/casos', data, {
+                headers: {
+                    authorization:ongId,
+                }
+            });
+
+            alert('Caso Cadastrado com sucesso');
+            history.push('/profile');
+        } catch (error) {
+            alert('falha ao cadastrar caso');
+        }
+    }
+
     return(
         <div className="novo-caso-container">
             <div className="content">
@@ -21,11 +54,23 @@ function NovoCaso() {
                         Voltar para home
                     </Link>
                 </section>
-                <form>
-                    <input placeholder="Títilo do caso"/>
-                    <textarea placeholder="Descrição"/>
+                <form onSubmit={handleNovoCaso}>
+                    <input 
+                        placeholder="Título do caso"
+                        valeu={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                    <textarea 
+                        placeholder="Descrição"
+                        valeu={description}
+                        onChange={e => setDescription(e.target.value)}
+                    />
 
-                    <input placeholder="Valor em reais"/>
+                    <input 
+                        placeholder="Valor em reais"
+                        valeu={value}
+                        onChange={e => setValue(e.target.value)}
+                    />
 
                     <button className="button" type="submit">Cadastrar</button>
                 </form>
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,78 +1,82 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import './styles.css';
+
+import api from '../../services/api';
+
 
 import logoImg from '../../assets/logo.svg';
 
 
 function Profile() {
+    const [casos, setCasos] = useState([]);
+
+    const ongId = localStorage.getItem('ongId');
+    const ongName = localStorage.getItem('ongName');
+
+    const history = useHistory();
+
+    useEffect(() => {
+        api.get('/profile', {
+            headers:{
+                authorization:ongId,
+            }
+        }).then(response => {
+            setCasos(response.data);
+        })
+    }, [ongId]);
+
+    async function handleDeleteCaso(id){
+        try {
+            await api.delete(`/casos/${id}`, {
+                headers: {
+                    authorization: ongId,
+                }
+            });
+
+            setCasos(casos.filter(caso => caso.id !== id));
+        } catch (error) {
+            alert('erro ao deletar caso');
+        }
+    }
+
+    function handleLogout() {
+        localStorage.clear();
+        history.push('/');
+    }
+
     return(
        <div className="profile-container">
            <header>
                 <img src={logoImg} alt="Be The Hero"/>
-                <span>Bem vinda, apad</span>
+                <span>Bem vinda, {ongName}</span>
 
                 <Link className="button" to="/casos/new">Cadastrar novo caso</Link>
-                <button type="button"> <FiPower size={18} color="#e02041"/></button>
+                <button onClick={handleLogout} type="button">
+                    <FiPower size={18} color="#e02041"/>
+                </button>
 
            </header>
 
            <h1>Casos cadastrados</h1>
 
            <ul>
-               <li>
-                   <strong>CASO:</strong>
-                   <p>caso teste</p>
+               {casos.map(caso => (
+                <li key={caso.id}>
+                    <strong>CASO:</strong>
+                    <p>{caso.title}</p>
 
-                   <strong>DESCRIÇÃO:</strong>
-                   <p>descrição teste</p>
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>{caso.description}</p>
 
-                   <strong>VALOR:</strong>
-                   <p>R$ 120,00</p>
+                    <strong>VALOR:</strong>
+                    <p>{Intl.NumberFormat('pt-BR', { style:'currency', currency: 'BRL'}).format(caso.value)}</p>
 
-                   <button type="button"> <FiTrash2 size={20} color="#a8a8b3"/></button>
-               </li>
-
-               <li>
-                   <strong>CASO:</strong>
-                   <p>caso teste</p>
-
-                   <strong>DESCRIÇÃO:</strong>
-                   <p>descrição teste</p>
-
-                   <strong>VALOR:</strong>
-                   <p>R$ 120,00</p>
-
-                   <button type="button"> <FiTrash2 size={20} color="#a8a8b3"/></button>
-               </li>
-
-               <li>
-                   <strong>CASO:</strong>
-                   <p>caso teste</p>
-
-                   <strong>DESCRIÇÃO:</strong>
-                   <p>descrição teste</p>
-
-                   <strong>VALOR:</strong>
-                   <p>R$ 120,00</p>
-
-                   <button type="button"> <FiTrash2 size={20} color="#a8a8b3"/></button>
-               </li>
-
-               <li>
-                   <strong>CASO:</strong>
-                   <p>caso teste</p>
-
-                   <strong>DESCRIÇÃO:</strong>
-                   <p>descrição teste</p>
-
-                   <strong>VALOR:</strong>
-                   <p>R$ 120,00</p>
-
-                   <button type="button"> <FiTrash2 size={20} color="#a8a8b3"/></button>
-               </li>
+                    <button onClick={() => handleDeleteCaso(caso.id)} type="button"> <FiTrash2 size={20} color="#a8a8b3"/></button>
+                </li>
+               ))}
            </ul>
        </div>
     );
